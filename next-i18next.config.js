047087b9a97fab5f153106db1c3428f87b8f5328
Miguel Gavilán <miangame1@gmail.com>
2025-02-28 16:18:38 +0100

    fix: i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'fr']
-  }
+  },
+  localePath:
+    typeof window === 'undefined'
+      ? require('path').resolve('./public/locales')
+      : './public/locales'
 }
