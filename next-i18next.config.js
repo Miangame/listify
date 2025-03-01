fb96a00d826e1342a1c7de4a488c6e85ebd42e53
Miguel Gavilán <miangame1@gmail.com>
2025-03-01 12:57:25 +0100

    fix: locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'fr'],
-    localePath: path.resolve('./public/locales')
-  }
+    locales: ['en', 'es', 'fr']
+  },
+  localePath: path.resolve('./public/locales')
 }
