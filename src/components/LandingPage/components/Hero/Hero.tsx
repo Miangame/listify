67e82f9887724095445f9241f035ab42a5548bdc
Miguel Gavilán <miangame1@gmail.com>
2025-03-01 13:18:16 +0100

    feat: add login to cta button

--- src/components/LandingPage/components/Hero/Hero.tsx
+++ src/components/LandingPage/components/Hero/Hero.tsx
@@ -1,16 +1,21 @@
 import { BiChevronRight } from 'react-icons/bi'
 import { useTranslation } from 'next-i18next'
+import { signIn } from 'next-auth/react'
 
 import { CTAButton, HeroSubtitle, HeroTitle, Wrapper } from './Hero.styled'
 
 export const Hero = () => {
   const { t } = useTranslation('landing')
 
+  const handleLogin = () => {
+    signIn('spotify')
+  }
+
   return (
     <Wrapper>
       <HeroTitle>{t('heroTitle')}</HeroTitle>
       <HeroSubtitle>{t('heroSubtitle')}</HeroSubtitle>
-      <CTAButton>
+      <CTAButton onClick={handleLogin}>
         {t('ctaButton')}
         <BiChevronRight size={16} />
       </CTAButton>
